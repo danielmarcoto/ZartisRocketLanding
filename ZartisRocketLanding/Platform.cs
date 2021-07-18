54a568d0bda85c93c994856eabdfea6e7db6e4f1
Daniel Marcoto <danielmarcoto@MacBook-Air-de-Daniel.local>
2021-07-18 16:03:55 -0300

    fix(Platform): corrects the condition for clash

--- ZartisRocketLanding/Platform.cs
+++ ZartisRocketLanding/Platform.cs
@@ -2,13 +2,13 @@ namespace ZartisRocketLanding
 {
     public class Platform
     {
-        private Position _lastRocketPosition;
+        private Rocket _lastRocket;
         public Platform(Position position, int width, int height) 
         {
             Position = position;
             Width = width;
             Height = height;
-            _lastRocketPosition = null;
+            _lastRocket = null;
         }
         public Position Position { get; set; }
         public int Width { get; set; }
@@ -16,13 +16,30 @@ namespace ZartisRocketLanding
 
         public LandingQueryReply QueryTrajectory(Rocket rocket) {
 
-            // Ok for Landing condition
-            if((rocket.Position.X >= Position.X 
-                && rocket.Position.X < (Position.X + Width))
-                && rocket.Position.Y >= Position.Y 
-                && rocket.Position.Y < (Position.Y + Height))
-                return LandingQueryReply.OkForLanding;
+            // Checks the last rocket position
+            if(_lastRocket != null && _lastRocket != rocket)
+            {
+                if((rocket.Position.X == _lastRocket.Position.X 
+                    || rocket.Position.X == (_lastRocket.Position.X + 1)
+                    || rocket.Position.X == (_lastRocket.Position.X - 1)) 
+                    && (rocket.Position.Y == _lastRocket.Position.Y 
+                    || rocket.Position.Y == (_lastRocket.Position.Y + 1)
+                    || rocket.Position.Y == (_lastRocket.Position.Y - 1)))
+                    return LandingQueryReply.Clash;
+            }
             
+            // Updates last rocket
+            _lastRocket = rocket;
+
+            // Ok for Landing condition
+            if(rocket.Position.X >= Position.X 
+                && rocket.Position.X < (Position.X + Width)
+                && rocket.Position.Y >= Position.Y 
+                && rocket.Position.Y < (Position.Y + Height)) 
+            {
+                return LandingQueryReply.OkForLanding;
+            }
+
             return LandingQueryReply.OutOfPlaform;
         }
     }
